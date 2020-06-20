75099b4ec1cb510fa96f553de5cfe08492f61238
ArielOrtega <arielortegabrenes98@gmail.com>
2020-06-20 15:51:08 -0600

    Updated units buy method

--- src/routes/guerrilla.js
+++ src/routes/guerrilla.js
@@ -79,9 +79,8 @@ router.post('/:name', async (req, res) => {
 
 //Se utiliza para comprar unidades
 router.put('/:name/units', async (req, res) => {
-    const { name, units } = req.params;
-    const { bunker } = req.body;
-    //console.log(bunker)
+    const { name } = req.params;
+    const { assault, engineer, tank, bunker } = req.body;
     const request = pool.request()
     request.input('name', sql.VarChar, name)
     const data = await request.execute('GetGuerrilla');
@@ -89,16 +88,29 @@ router.put('/:name/units', async (req, res) => {
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     output = parser(data)
     array = JSON.parse(output)
-    console.log(array.army.assault)
-
-    if (Object.keys(data.recordsets).length > 0) {
-        res.status(200);
-        res.statusMessage = 'successful transaction'
-        res.json(JSON.parse(output));
-    } else {
-        res.status(404);
-        res.statusMessage = 'Not found'
-        res.json(output);
+    resources = calculateBuy(assault, engineer, tank, bunker);
+    if( resources.moneyRequired <= array.resources.money && resources.oilRequired <= array.resources.oil && 
+        resources.peopleRequired <= array.resources.people){
+        console.log('compra realizada')
+        const request2 = pool.request()
+        request2.input('name', sql.VarChar, name)
+        request2.input('money', sql.Int, resources.moneyRequired)
+        request2.input('oil', sql.Int, resources.oilRequired)
+        request2.input('people', sql.Int, resources.peopleRequired)
+        request2.input('assault', sql.Int, assault)
+        request2.input('tank', sql.Int, tank)
+        request2.input('engineer', sql.Int, engineer)
+        request2.input('bunker', sql.Int, bunker)
+        const updatedGuerrilla = await request2.execute('updateResources')
+        console.log(updatedGuerrilla)
+        res.status(200)
+        res.statusMessage='Successful transaction'
+        res.json(JSON.parse(parser(updatedGuerrilla)))
+    }else{
+        console.log('fondos insuficientes')
+        res.status(402)
+        res.statusMessage = 'Non-sufficient funds'
+        res.json(array)
     }
     res.status(400);
     res.statusMessage = 'Bad request'
@@ -121,4 +133,37 @@ function parser(data) {
         }
     }
     return newResults
+}
+
+function calculateBuy(assault, engineer, tank, bunker){
+    resources = {
+        moneyRequired:0,
+        oilRequired:0,
+        peopleRequired:0
+    }
+    //recursos requeridos para los asalto
+    for (let i = 0; i < assault; i++) {
+        resources.moneyRequired += 20
+        resources.oilRequired += 25
+        resources.peopleRequired += 1
+    }
+    //recursos requeridos para los ingenieros
+    for (let i = 0; i < engineer; i++) {
+        resources.moneyRequired += 10
+        resources.oilRequired += 25
+        resources.peopleRequired += 1
+    }
+    //recursos requeridos para los tanques
+    for (let i = 0; i < tank; i++) {
+        resources.moneyRequired += 200
+        resources.oilRequired += 500
+        resources.peopleRequired += 8
+    }
+    //recursos requeridos para los bunker
+    for (let i = 0; i < bunker; i++) {
+        resources.moneyRequired += 300
+        resources.oilRequired += 200
+        resources.peopleRequired += 8
+    }
+    return resources;
 }
