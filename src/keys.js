6280faf1989d39dd528fa2ef5d4b9baedeeee463
ArielOrtega <arielortegabrenes98@gmail.com>
2020-06-29 15:46:26 -0600

    Updated db keys

--- src/keys.js
+++ src/keys.js
@@ -2,10 +2,10 @@ module.exports = {
 
     database: {
         connectionLimit: 10,
-        server: '163.178.107.10',
+        server: '163.178.173.148',
         port: 1433,
-        user: 'laboratorios',
-        password: 'Saucr.118',
+        user: 'estudiantesrp',
+        password: 'estudiantesrp',
         database: '2020_IF5000_E2-C'
     }
 };
